f605c71947f3b4042d805e8b5ff8e3d26570410e
Paa-Kofi Aidoo <paakofiaidoo17@gmail.com>
2025-10-25 23:32:44 +0000

    chore: remove all project files and reset workspace.

--- .juki/engine/data/dtos/dto.go
+++
@@ -1,9 +0,0 @@
-package dtos
-
-type PageDto struct {
-	Body struct {
-		Name        string `json:"name" docs:"page name"`
-		Route       string `json:"route"`
-		CustomTheme bool   `json:"custom_theme"`
-	}
-}
